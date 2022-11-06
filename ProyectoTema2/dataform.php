5a8b82c2b8608fadcb359da5eddf7b9c386ea64c
Sulaiman Taieb Hadouchi <113556106+suli9828@users.noreply.github.com>
2022-11-06 17:06:30 +0100

    Add files via upload

--- ProyectoTema2/dataform.php
+++ ProyectoTema2/dataform.php
@@ -1,15 +1,30 @@
-<?php
-if (isset($_POST['enviar'])) {
-    $nombre = $_POST['nombre'];
-    echo $nombre;
-    echo "<br>";
-    $apellidos = $_POST['apellidos'];
-    echo $apellidos;
-    echo "<br>";
-    $direccion = $_POST['direccion'];
-    echo $direccion;
-    echo "<br>";
-    $telefono = $_POST['telefono'];
-    echo $telefono;
-    echo "<br>";
-}
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    if (isset($_POST['enviar'])) {
+        $name = $_POST['name'];
+        echo "Name of the album: " . $name;
+        echo "<br>";
+        $artist = $_POST['artist'];
+        echo "Artist/s: " . $artist;
+        echo "<br>";
+        $price = $_POST['price'];
+        echo "Price: " . $price;
+        echo "<br>";
+        $fee = $_POST['fee'];
+        echo "Fee: " . $fee;
+        echo "<br>";
+    }
+    ?>
+</body>
+
+</html>
