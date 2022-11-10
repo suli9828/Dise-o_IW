f55cea1c3e6a9101a34779d67cf2e47bf69ad6de
Sulaiman Taieb Hadouchi <113556106+suli9828@users.noreply.github.com>
2022-11-10 01:26:26 +0100

    Add files via upload

--- ProyectoTema2/dataform.php
+++ ProyectoTema2/dataform.php
@@ -5,25 +5,69 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Album and user details</title>
 </head>
 
 <body>
     <?php
-    if (isset($_POST['enviar'])) {
-        $name = $_POST['name'];
-        echo "Name of the album: " . $name;
-        echo "<br>";
-        $artist = $_POST['artist'];
-        echo "Artist/s: " . $artist;
-        echo "<br>";
-        $price = $_POST['price'];
-        echo "Price: " . $price;
-        echo "<br>";
-        $fee = $_POST['fee'];
-        echo "Fee: " . $fee;
-        echo "<br>";
-    }
+    $name = $_POST['name'];
+    echo "Name of the album: " . $name;
+    echo "<br>";
+    echo "<br>";
+
+    $artist = $_POST['artist'];
+    echo "Artist/s: " . $artist;
+    echo "<br>";
+    echo "<br>";
+
+    $price = $_POST['price'];
+    echo "Price: " . $price;
+    echo "<br>";
+    echo "<br>";
+
+    $fee = $_POST['fee'];
+    echo "Fee: " . $fee;
+    echo "<br>";
+    echo "<br>";
+
+    $description = $_POST['description'];
+    echo "Description of the album: " . $description;
+    echo "<br>";
+    echo "<br>";
+
+    $type = $_POST['type'];
+    echo "Type of music album: " . $type;
+    echo "<br>";
+    echo "<br>";
+
+    $address = $_POST['address'];
+    echo "Address: " . $address;
+    echo "<br>";
+    echo "<br>";
+
+    $email = $_POST['email'];
+    echo "Email: " . $email;
+    echo "<br>";
+    echo "<br>";
+
+    $phonenumber = $_POST['phonenumber'];
+    echo "Phone number: " . $phonenumber;
+    echo "<br>";
+    echo "<br>";
+
+    $webpage = $_POST['webpage'];
+    echo "Your webpage: " . $webpage;
+    echo "<br>";
+    echo "<br>";
+
+    $o1 = $_POST['o1'];
+    echo "Favourite music type: " . $o1;
+    echo "<br>";
+    echo "<br>";
+
+    $opinion = $_POST['opinion'];
+    echo "Your opinion: " . $opinion;
+    echo "<br>";
     ?>
 </body>
 
